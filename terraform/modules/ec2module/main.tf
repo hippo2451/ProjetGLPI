fe754e8e7671e95b71b4af805835da6f318eecde
hippo2451 <79691884+hippo2451@users.noreply.github.com>
2022-11-28 09:33:33 +0100

    Create main.tf

---
+++ terraform/modules/ec2module/main.tf
@@ -0,0 +1,64 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+
+resource "aws_security_group" "allow_ssh_http_https" {
+  name        = var.sg_name
+  description = "Allow http, https inbound traffic"
+
+  ingress {
+    description = "http from vpc"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https from vpc"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "ssh from vpc"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "myec2" {
+  ami             = data.aws_ami.app_ami.id
+  instance_type   = var.instancetype
+  key_name        = "devops"
+  tags            = var.aws_common_tag
+  security_groups = ["${aws_security_group.allow_ssh_http_https.name}"]
+  provisioner "local-exec" {
+    command = "echo PUBLIC IP: ${aws_instance.myec2.public_ip} ; ID :${aws_instance.myec2.id} ; AZ: ${aws_instance.myec2.availability_zone} >> infos.ec2.txt"
+  }
+  
+
+}
+
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.myec2.id
+  vpc      = true
+}
+
